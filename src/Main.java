ac861786f7e2b80b384e65d35db1fee76a3451c3
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 08:34:07 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Hitta största gemensamma delaren till två heltal m och n
+        // Loopa
+        //   r = m % n
+        //   om r = 0 --> klara och resultatet finns i n
+        //   m = n
+        //   n = r
+
+
+    }
+}
